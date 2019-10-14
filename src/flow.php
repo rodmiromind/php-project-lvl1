e3d0de2bd09bb449187be53017a9a44c743d6ca3
notebook <onetreearmy@gmail.com>
2019-10-14 22:29:04 +0300

    Step 5

---
+++ src/flow.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BrainGames\Flow;
+
+use function cli\line;
+
+function flow($gameRules, $questions, $correctAnswers)
+{
+    line('Welcome to the Brain Games!');
+    $gameRules;
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
+
+    for ($i = 0; $i < 3; $i++) {
+        line("Question: {$questions[$i]}");
+        $answer = \cli\prompt('Your answer');
+
+        if ($answer == $correctAnswers[$i]) {
+            line("Correct!");
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswers[$i]}'.
+            Let's try again, {$name}!");
+            exit;
+        }
+    }
+
+    line("Congratulations, {$name}!");
+}
---
+++ src/games/BrainEven.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BrainGames\BrainEven;
+
+use function cli\line;
+use function BrainGames\Flow\flow;
+function run() {
+
+function findCorrectAnswer($questions)
+{
+    $correctAnswers =[];
+    foreach ($questions as $question) {
+        $correctAnswers[] = $question % 2 === 0 ? 'yes' : 'no';
+    }
+    return $correctAnswers;
+}
+
+ $gameRules = line('Answer "yes" if number even otherwise answer "no".');
+$questions = [random_int(1, 100),random_int(1, 100),random_int(1, 100)];
+   $correctAnswers = findCorrectAnswer($questions);
+
+flow($gameRules, $questions, $correctAnswers);
+}
