0c60228e9872e25d92a78e188363e7e2d63c463c
notebook <onetreearmy@gmail.com>
2019-10-14 22:35:11 +0300

    Step 5

--- src/games/BrainCalc.php
+++ src/games/BrainCalc.php
@@ -5,33 +5,34 @@ namespace BrainGames\BrainCalc;
 use function cli\line;
 use function BrainGames\Flow\flow;
 
-function run() {
-
-function getQuestionsAndAnswers()
+function run()
 {
-    $correctAnswers = [];
-    $questions = [];
-    for ($i = 0; $i < 3; $i++) {
-        $num1 = random_int(1, 100);
-        $num2 = random_int(1, 100);
-        $operators = ['-', '+', '*'];
-        shuffle($operators);
-        $questions[] = "{$num1}{$operators[0]}{$num2}";
 
-        switch ($operators[0]) {
-            case '-':
-                $correctAnswers[] = $num1 - $num2;
-                break;
-            case '+':
-                $correctAnswers[] = $num1 + $num2;
-                break;
-            case '*':
-                $correctAnswers[] = $num1 * $num2;
-                break;
+    function getQuestionsAndAnswers()
+    {
+        $correctAnswers = [];
+        $questions = [];
+        for ($i = 0; $i < 3; $i++) {
+            $num1 = random_int(1, 100);
+            $num2 = random_int(1, 100);
+            $operators = ['-', '+', '*'];
+            shuffle($operators);
+            $questions[] = "{$num1}{$operators[0]}{$num2}";
+
+            switch ($operators[0]) {
+                case '-':
+                    $correctAnswers[] = $num1 - $num2;
+                    break;
+                case '+':
+                    $correctAnswers[] = $num1 + $num2;
+                    break;
+                case '*':
+                    $correctAnswers[] = $num1 * $num2;
+                    break;
+            }
         }
+        return array_merge($questions, $correctAnswers);
     }
-    return array_merge($questions, $correctAnswers);
-}
 
 $gameRules = line('What is the result of the expression?');
 $questionsAndAnswers = getQuestionsAndAnswers();
